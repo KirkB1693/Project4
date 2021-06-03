ed033feaa393521ce53920798b2832166fe796f6
KirkB1693 <kirkblomstrom@prodigy.net>
2021-06-03 09:31:51 -0700

    Initial commit

---
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -0,0 +1,122 @@
+package com.udacity.project4.authentication
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.Observer
+import androidx.lifecycle.map
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.AuthMethodPickerLayout
+import com.udacity.project4.R
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
+
+/**
+ * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
+ * signed in users to the RemindersActivity.
+ */
+class AuthenticationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthenticationBinding
+
+    companion object {
+        const val TAG = "Authentication Activity"
+        const val SIGN_IN_REQUEST_CODE = 1001
+    }
+
+    enum class AuthenticationState {
+        AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
+    }
+
+    private val authenticationState = FirebaseUserLiveData().map { user ->
+        if (user != null) {
+            AuthenticationState.AUTHENTICATED
+        } else {
+            AuthenticationState.UNAUTHENTICATED
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthenticationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener { launchSignInFlow() }
+        observeAuthenticationState()
+    }
+
+    private fun observeAuthenticationState() {
+
+        authenticationState.observe(this, Observer { authenticationState ->
+            when (authenticationState) {
+                AuthenticationState.AUTHENTICATED -> {
+                    Log.i(TAG, "User is logged in")
+                    launchRemindersActivity()
+                }
+                else -> {
+                    Log.i(TAG, "User is logged out")
+                }
+            }
+        })
+    }
+
+    private fun launchRemindersActivity() {
+        val remindersIntent = Intent(this, RemindersActivity::class.java)
+        startActivity(remindersIntent)
+        finish()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // User successfully signed in
+                Log.i(TAG, "Successfully signed in user ${FirebaseAuth.getInstance().currentUser?.displayName}!")
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
+
+    private fun launchSignInFlow() {
+        // Give users the option to sign in / register with their email or Google account.
+        // If users choose to register with their email,
+        // they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+
+            // This is where you can provide more ways for users to register and
+            // sign in.
+        )
+
+        val customLayout = AuthMethodPickerLayout.Builder(R.layout.authentication_method)
+            .setGoogleButtonId(R.id.google_button)
+            .setEmailButtonId(R.id.email_button)
+            .build()
+
+        // Create and launch sign-in intent.
+        // We listen to the response of this activity with the
+        // SIGN_IN_REQUEST_CODE
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setAuthMethodPickerLayout(customLayout)
+                .build(),
+            SIGN_IN_REQUEST_CODE
+        )
+    }
+
+
+}
---
+++ app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -0,0 +1,80 @@
+package com.udacity.project4.locationreminders
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.map
+import androidx.navigation.fragment.NavHostFragment
+import com.firebase.ui.auth.AuthUI
+import com.udacity.project4.R
+import com.udacity.project4.authentication.AuthenticationActivity
+import com.udacity.project4.authentication.FirebaseUserLiveData
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import kotlinx.android.synthetic.main.activity_reminders.*
+
+/**
+ * The RemindersActivity that holds the reminders fragments
+ */
+class RemindersActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "Reminders Activity"
+    }
+
+    enum class AuthenticationState {
+        AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
+    }
+
+    private val authenticationState = FirebaseUserLiveData().map { user ->
+        if (user != null) {
+            AuthenticationState.AUTHENTICATED
+        } else {
+            AuthenticationState.UNAUTHENTICATED
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reminders)
+        observeAuthenticationState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                (nav_host_fragment as NavHostFragment).navController.popBackStack()
+                return true
+            }
+            R.id.logout -> {
+                AuthUI.getInstance().signOut(applicationContext)
+                // launchAuthenticationActivity()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun launchAuthenticationActivity() {
+        val authIntent = Intent(this, AuthenticationActivity::class.java)
+        startActivity(authIntent)
+        finish()
+    }
+
+    private fun observeAuthenticationState() {
+
+        authenticationState.observe(this, Observer { authenticationState ->
+            when (authenticationState) {
+                AuthenticationState.AUTHENTICATED -> {
+                    Log.i(TAG, "User is logged in")
+                }
+                else -> {
+                    Log.i(TAG, "User is logged out")
+                    launchAuthenticationActivity()
+                }
+            }
+        })
+    }
+}
