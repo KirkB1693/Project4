cba58acbb354924ed00d79127b6c76eaa1cd11d0
KirkB1693 <kirkblomstrom@prodigy.net>
2021-06-08 18:52:44 -0700

    App runs as expected, unit testing complete, still need to add instrumented testing

--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,99 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.provider.CalendarContract
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
+    private lateinit var remindersListViewModel: RemindersListViewModel
+    private lateinit var dataSource: FakeDataSource
+    private var remindersList = mutableListOf<ReminderDTO>()
+
+    @Before
+    fun setupViewModel() {
+        stopKoin()
+        val reminder1 = ReminderDTO("Googleplex", "Check it out", "Googleplex", 37.4221, -122.0841)
+        val reminder2 = ReminderDTO("Coffee", "Get a latte", "Starbucks", 37.4158, -122.0776)
+        val reminder3 = ReminderDTO("Donuts", "Pick up a dozen", "Krispy Kreme", 37.4193, -122.0943)
+        remindersList.addAll(arrayListOf(reminder1, reminder2, reminder3))
+        dataSource = FakeDataSource(remindersList)
+        remindersListViewModel =
+            RemindersListViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+    }
+
+    @Test
+    fun loadReminders_threeRemindersInList_remindersListIsCorrect() = runBlockingTest {
+        // when loading the reminders
+        remindersListViewModel.loadReminders()
+
+        // then reminders from the list should equal reminders loaded
+        val value = remindersListViewModel.remindersList.getOrAwaitValue()
+        assertEquals(remindersList.size, value.size)
+        assertEquals(remindersList[0].title, value[0].title)
+        assertEquals(remindersList[0].description, value[0].description)
+        assertEquals(remindersList[0].location, value[0].location)
+        assertEquals(remindersList[0].latitude, value[0].latitude)
+        assertEquals(remindersList[0].longitude, value[0].longitude)
+        assertEquals(remindersList[0].id, value[0].id)
+        assertEquals(remindersList[1].title, value[1].title)
+        assertEquals(remindersList[1].description, value[1].description)
+        assertEquals(remindersList[1].location, value[1].location)
+        assertEquals(remindersList[1].latitude, value[1].latitude)
+        assertEquals(remindersList[1].longitude, value[1].longitude)
+        assertEquals(remindersList[1].id, value[1].id)
+        assertEquals(remindersList[2].title, value[2].title)
+        assertEquals(remindersList[2].description, value[2].description)
+        assertEquals(remindersList[2].location, value[2].location)
+        assertEquals(remindersList[2].latitude, value[2].latitude)
+        assertEquals(remindersList[2].longitude, value[2].longitude)
+        assertEquals(remindersList[2].id, value[2].id)
+    }
+
+    @Test
+    fun loadReminders_emptyList_remindersListIsCorrect() = runBlockingTest {
+        // Given empty data source
+        dataSource.deleteAllReminders()
+        remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+        // when loading the reminders
+        remindersListViewModel.loadReminders()
+        // then reminders loaded should be empty
+        assertEquals(0, remindersListViewModel.remindersList.value?.size)
+
+    }
+
+    @Test
+    fun loadReminders_nullList_errorReturnedAndSnackbarShown() = runBlockingTest {
+        // Given a null data source
+        dataSource = FakeDataSource(null)
+        remindersListViewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+        // when loading the reminders
+        remindersListViewModel.loadReminders()
+        // then error should be returned and snackbar shown with not found message
+        val error = remindersListViewModel.showSnackBar.getOrAwaitValue()
+        assert(error.contains("Reminders not found"))
+    }
 }
--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,16 +1,176 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PointOfInterest
+import com.udacity.project4.R
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.locationreminders.getOrAwaitValue
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.robolectric.RuntimeEnvironment.application
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 class SaveReminderViewModelTest {
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
+    private lateinit var dataSource: FakeDataSource
+    private var remindersList = mutableListOf<ReminderDTO>()
+
+    @Before
+    fun setupViewModel() {
+        stopKoin()
+        val reminder1 = ReminderDTO("Googleplex", "Check it out", "Googleplex", 37.4221, -122.0841)
+        val reminder2 = ReminderDTO("Coffee", "Get a latte", "Starbucks", 37.4158, -122.0776)
+        val reminder3 = ReminderDTO("Donuts", "Pick up a dozen", "Krispy Kreme", 37.4193, -122.0943)
+        remindersList.addAll(arrayListOf(reminder1, reminder2, reminder3))
+        dataSource = FakeDataSource(remindersList)
+        saveReminderViewModel =
+            SaveReminderViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+    }
 
 
-    //TODO: provide testing to the SaveReminderView and its live data objects
+    @Test
+    fun clearData_dataSourceIsEmpty() {
+        // when data is cleared
+        saveReminderViewModel.onClear()
 
+        // then the values should all be null
+        assertEquals(null, saveReminderViewModel.reminderTitle.value)
+        assertEquals(null, saveReminderViewModel.reminderDescription.value)
+        assertEquals(null, saveReminderViewModel.reminderSelectedLocationStr.value)
+        assertEquals(null, saveReminderViewModel.latitude.value)
+        assertEquals(null, saveReminderViewModel.longitude.value)
+        assertEquals(null, saveReminderViewModel.selectedPOI.value)
+    }
 
+    @Test
+    fun validateAndSaveReminder_validReminder_reminderAddedToDataSource() = runBlockingTest {
+        // when valid reminder is added
+        val reminderToAdd = ReminderDataItem(
+            "Golfing",
+            "Hit a dozen practice balls on the driving range",
+            "Shoreline Golf Links",
+            37.4306,
+            -122.0855
+        )
+
+        saveReminderViewModel.validateAndSaveReminder(reminderToAdd)
+
+        // then reminder should now be part of data and the saved toast should show
+        val result = saveReminderViewModel.dataSource.getReminder(reminderToAdd.id)
+        val data: ReminderDTO? = when (result) {
+            is Result.Success<*> -> {
+                result.data as ReminderDTO
+            }
+            else -> null
+        }
+        val toast = saveReminderViewModel.showToast.getOrAwaitValue()
+        assertNotEquals(null, data)
+        assertEquals(reminderToAdd.title, data?.title)
+        assertEquals(reminderToAdd.description, data?.description)
+        assertEquals(reminderToAdd.location, data?.location)
+        assertEquals(reminderToAdd.latitude, data?.latitude)
+        assertEquals(reminderToAdd.longitude, data?.longitude)
+        assertEquals(reminderToAdd.id, data?.id)
+        assert(toast.contains("Reminder Saved"))
+
+    }
+
+    @Test
+    fun validateAndSaveReminder_invalidReminderNoTitle_reminderNotAddedToDataSourceAndSnackBarShows() = runBlockingTest {
+        // when invalid reminder is added
+        val reminderToAdd = ReminderDataItem(
+            "",
+            "Hit a dozen practice balls on the driving range",
+            "Shoreline Golf Links",
+            37.4306,
+            -122.0855
+        )
+
+        saveReminderViewModel.validateAndSaveReminder(reminderToAdd)
+
+        // then reminder should not be part of data and the snackbar should show to prompt user for valid input
+        val result = saveReminderViewModel.dataSource.getReminder(reminderToAdd.id)
+        val error: Boolean = when (result) {
+            is Result.Error -> {
+                true
+            }
+            else -> false
+        }
+        val snackbarInt = saveReminderViewModel.showSnackBarInt.getOrAwaitValue()
+        assertEquals(true, error)
+        assertEquals(R.string.err_enter_title, snackbarInt)
+    }
+
+    @Test
+    fun validateAndSaveReminder_invalidReminderNoLocation_reminderNotAddedToDataSourceAndSnackBarShows() = runBlockingTest {
+        // when invalid reminder is added
+        val reminderToAdd = ReminderDataItem(
+            "Shoreline Golf Links",
+            "Hit a dozen practice balls on the driving range",
+            "",
+            0.0,
+            0.0
+        )
+
+        saveReminderViewModel.validateAndSaveReminder(reminderToAdd)
+
+        // then reminder should not be part of data and the snackbar should show to prompt user for valid input
+        val result = saveReminderViewModel.dataSource.getReminder(reminderToAdd.id)
+        val error: Boolean = when (result) {
+            is Result.Error -> {
+                true
+            }
+            else -> false
+        }
+        val snackbarInt = saveReminderViewModel.showSnackBarInt.getOrAwaitValue()
+        assertEquals(true, error)
+        assertEquals(R.string.err_select_location, snackbarInt)
+    }
+
+    @Test
+    fun setPOI_validPOI_valuesAreSet() {
+        // when a valid POI is passed in
+        val poi = PointOfInterest(LatLng(37.4306, -122.0855), "", "Shoreline Golf Links")
+        // then the latitude, longitude and name values should be set in the viewmodel
+        saveReminderViewModel.setPoi(poi)
+        assertEquals(poi.name, saveReminderViewModel.reminderSelectedLocationStr.value)
+        assertEquals(poi.latLng.latitude, saveReminderViewModel.latitude.value)
+        assertEquals(poi.latLng.longitude, saveReminderViewModel.longitude.value)
+    }
+
+    @Test
+    fun setPOI_invalidPOI_valuesAreNotSet() {
+        // when an invalid POI is passed in
+        val poi = null
+        // then the latitude, longitude and name values should not be set in the viewmodel
+        saveReminderViewModel.setPoi(poi)
+        assertEquals(null, saveReminderViewModel.reminderSelectedLocationStr.value)
+        assertEquals(null, saveReminderViewModel.latitude.value)
+        assertEquals(null, saveReminderViewModel.longitude.value)
+    }
 }
